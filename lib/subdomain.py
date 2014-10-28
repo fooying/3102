10968aca21bc8d8469b1660312e388f457c3cd94
zhangzy <zhangzy@knownsec.com>
2014-10-28 18:00:38 +0800

    增加获取子域模块

---
+++ lib/subdomain.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Copyright (c) 2014 Fooying (http://www.fooying.com)
+Mail:f00y1n9[at]gmail.com
+"""
+
+import re
+import request import request
+from fdomain.rootdomain import Domain
+
+
+class SubDomain:
+    def __init__(self, source_name="links"):
+        self.source_name = source_name
+
+    def get_subdomain_by_links(self, domain, level=4):
+        domain = Domain.get_domain(domain)
+        url = 'http://i.links.cn/subdomain/'
+        data = {
+            'domain': domain,
+            'b2': 1,
+            'b3': 1 if level>=3 else 0,
+            'b4': 1 if level>=4 else 0,
+        }
+        html = request(url, 'POST', data=data)
+        regex = '''<a\shref="http://[^"]*?"\srel=nofollow\starget=_blank>http://([^"]*?)</a></div>'''
+        try:
+            result = re.findall(regex, html)
+        except:
+            result = []
+            time.sleep(3)
+        result.append(domain)
+        return list(set(result))
