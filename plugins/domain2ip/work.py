7566f148d2d2494d9eba62b770db8f7449d3b695
fooying <f00y1n9@gmail.com>
2014-12-21 18:58:36 +0800

    增加domain2ip插件

---
+++ plugins/domain2ip/work.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Copyright (c) 2014 Fooying (http://www.fooying.com)
+Mail:f00y1n9[at]gmail.com
+"""
+
+import socket
+
+from core.plugin import Plugin
+
+
+class domain2ip(Plugin):
+    def start(self, target, domain_type, level):
+        try:
+            ip = socket.gethostbyname(target)
+            result = {
+                'result': {
+                    'root_domain': [],
+                    'ip': [ip],
+                    'domain': []
+                },
+                'module': 'domain2ip',
+                'parent_target': target,
+                'level': level,
+            }
+        except socket.gaierror:
+            result = None
+        return result
