50e81f05b372a9382227784153323aa05bb1bf19
fooying <f00y1n9@gmail.com>
2014-12-21 19:00:02 +0800

    增加ip2domain插件

---
+++ plugins/ip2domain/work.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Copyright (c) 2014 Fooying (http://www.fooying.com)
+Mail:f00y1n9[at]gmail.com
+"""
+
+import re
+import time
+
+from core.plugin import Plugin
+from comm.utils import get_domain_type
+
+
+class ip2domain(Plugin):
+    def __get_count(self, html):
+        total_page_regx = (
+            r'''<span\sclass="sb_count">\d*?\s-\s\d*?'''
+            r'''\s[^\(]*?\([^\s]*?\s(\d*?)\s[^\)]*?\)</span>'''
+        )
+        result = re.search(total_page_regx, html)
+        try:
+            total_num = int(result.group(1).replace(',', ''))
+        except:
+            total_num = 9
+        page_count = total_num / 9
+        if total_num % 9 > 0:
+            page_count += 1
+        return total_num, page_count
+
+    def __classify_result(self, domain_list):
+        domains = []
+        root_domains = []
+        ips = []
+        for domain in domain_list:
+            domain_type = get_domain_type(domain)
+            if domain_type == 'ip' and domain not in ips:
+                ips.append(domain)
+            elif domain_type == 'root_domain' and domain not in root_domains:
+                root_domains.append(domain)
+            elif domain_type == 'domain' and domain not in domains:
+                domains.append(domain)
+        return domains, root_domains, ips
+
+    def start(self, target, domain_type, level):
+        url = ('http://cn.bing.com/search?q='
+               'ip:%s&first=999999991&FORM=PERE' % target)
+        html = self.req.request('GET', url).text
+
+        domain_regx = r'''
+            <h2><a\shref="https?://([^"]*?)"\starget="_blank"\sh="ID=[^"]*?">[^<]*?</a></h2>
+        '''
+        domain_list = re.findall(domain_regx, html, re.X)
+
+        total_num, page_count = self.__get_count(html)
+
+        if page_count > 0:
+            for n in range(total_num-1):
+                url = ('http://cn.bing.com/search?q='
+                       'ip:%s&first=%s1&FORM=PERE3' % (target, n))
+                html = self.req.request('GET', url).text
+                new_domain_list = re.findall(domain_regx, html, re.X)
+                domain_list.extend(new_domain_list)
+                time.sleep(1)
+
+        domains, root_domains, ips = self.__classify_result(domain_list)
+        result = {
+            'result': {
+                'root_domain': root_domains,
+                'ip': ips,
+                'domain': domains
+            },
+            'module': 'ip2domain',
+            'parent_target': target,
+            'level': level,
+        }
+        return result
