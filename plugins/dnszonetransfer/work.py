0e5fb2cb3d964d838ef052fc9d7ff43b23a71af0
fooying <f00y1n9@gmail.com>
2014-12-24 17:41:53 +0800

    增加容错处理

--- plugins/dnszonetransfer/work.py
+++ plugins/dnszonetransfer/work.py
@@ -17,45 +17,58 @@ class dnszonetransfer(Plugin):
 
     def start(self, target, domain_type, level):
         super(dnszonetransfer, self).start(target, domain_type, level)
-        resolver = DnsHelper(target)
-        mx_list = resolver.get_mx()
-        soa_list = resolver.get_soa()
-        txt_list = resolver.get_txt()
-        spf_list = resolver.get_spf()
-        transfer_list = resolver.zone_transfer()
-        record_lists = [mx_list, soa_list, txt_list, spf_list]
-
-        domains = []
-        ips = []
-        for record_list in record_lists:
-            if record_list:
-                for record in record_list:
-                    domains.append(record[1])
-                    ips.append(record[2])
-
-        if transfer_list:
-            for record in transfer_list:
-                if 'zone_server' in record:
-                    domains.append(record['name'])
-                    ips.append(record['zone_server'])
-                    ips.append(record['address'])
-                else:
-                    ips.append(record['ns_server'])
-
-        ips = list(set(ips))
-        domains = list(set(domains))
-        if domains or ips:
-            result = {
-                'result': {
-                    'root_domain': [],
-                    'ip': ips,
-                    'domain': domains,
-                },
-                'module': self.name,
-                'parent_target': target,
-                'level': level,
-            }
+        result = None
+        try:
+            resolver = DnsHelper(target)
+            mx_list = resolver.get_mx()
+            soa_list = resolver.get_soa()
+            txt_list = resolver.get_txt()
+            spf_list = resolver.get_spf()
+            transfer_list = resolver.zone_transfer()
+        except:
+            pass
         else:
-            result = None
+            record_lists = [mx_list, soa_list, txt_list, spf_list]
+
+            domains = []
+            ips = []
+            for record_list in record_lists:
+                if record_list:
+                    for record in record_list:
+                        domains.append(record[1])
+                        ips.append(record[2])
+
+            if transfer_list:
+                for record in transfer_list:
+                    if 'zone_server' in record:
+                        ips.append(record['zone_server'])
+                        if 'name' in record:
+                            domain = record['name']
+                        elif 'mname' in record:
+                            domain = record['mname']
+                        elif 'target' in record:
+                            domain = record['target']
+                        else:
+                            domain = ''
+                        if domain:
+                            domains.append(domain)
+                        if 'adderss':
+                            ips.append(record['address'])
+                    else:
+                        ips.append(record['ns_server'])
+
+            ips = list(set(ips))
+            domains = list(set(domains))
+            if domains or ips:
+                result = {
+                    'result': {
+                        'root_domain': [],
+                        'ip': ips,
+                        'domain': domains,
+                    },
+                    'module': self.name,
+                    'parent_target': target,
+                    'level': level,
+                }
         super(dnszonetransfer, self).end()
         return result
