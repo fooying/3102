7b2e3fc8b10e4e062a71ac57dcd25442b74179c4
fooying <f00y1n9@gmail.com>
2014-12-25 11:28:29 +0800

    增加对内网IP的判断,内网IP不做保存和任务下发

--- core/controllers/taskmanager.py
+++ core/controllers/taskmanager.py
@@ -13,6 +13,7 @@ import logging
 from core.data import kb
 from core.data import conf
 from core.data import result
+from comm.util import is_intra_ip
 from comm.rootdomain import Domain
 
 logger = logging.getLogger('3102')
@@ -20,48 +21,53 @@ logger = logging.getLogger('3102')
 
 def task_monitor(pc):
     while True:
-        print_task_status()
         try:
             one_result = pc.wp.result.get(timeout=1)
-        except gevent.queue.Empty, e:
+        except gevent.queue.Empty:
             if pc.wp.target_queue.empty() and pc.wp.is_finished():
-                pc.exit = True
+                pc.exit()
                 break
         else:
             add_task_and_save(pc, one_result)
 
 
-def print_task_status(log=False):
-    msg = 'Task monitor:level %s:task num: %s, result num: %s\r' % (
-        kb.status.level, kb.status.task_num, kb.status.result_num
+def print_task_status(pc):
+    status = pc.wp.get_status()
+    msg = ('Task Monitor:level %s:total task: %s ,'
+           'wait task: %s, result num: %s') % (
+        kb.status.level, kb.status.total_task_num,
+        kb.status.total_task_num - kb.status.do_task_num,
+        kb.status.result_num
     )
-    if log:
-        logger.debug(msg)
-    else:
-        print '\033[1;32m[+]%s\033[1;m\r' % msg,
+    msg = msg.ljust(78, ' ')
+    logger.debug(msg)
+    print status
 
 
 def add_task_and_save(pc, one_result):
     level = one_result.get('level', -1) + 1
-    module = one_result.get('module')
+
     if level <= conf.max_level:
         if level > kb.status.level:
             kb.status.level = level
-            print_task_status()
+            print_task_status(pc)
+
         module = one_result.get('module')
 
         for task_type in one_result.get('result', {}).keys():
             for domain in one_result.get('result', {}).get(task_type, []):
+                domain = Domain.url_format(domain)
+                if task_type == 'ip' and is_intra_ip(domain):
+                    continue
                 if domain not in result[task_type]:
-                    domain = Domain.url_format(domain)
                     target = {
                         'level': level,
                         'domain_type': task_type,
                         'target': domain,
                         'parent_module': module
                     }
+                    kb.status.total_task_num += 1
                     pc.wp.target_queue.put(target)
-                    kb.status.task_num += 1
                     save_result(one_result, domain, task_type)
 
 
@@ -71,3 +77,10 @@ def save_result(one_result, domain, task_type):
         want_save_result.pop('result')
         result[task_type][domain] = want_save_result
         kb.status.result_num += 1
+
+
+def start_job(pc):
+    while not pc.exit:
+        pc.run_job()
+        print_task_status(pc)
+        gevent.sleep(10)
