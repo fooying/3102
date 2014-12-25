29e4abdcd8b8c529e9a8b9bc37e8a41d0e51c60a
fooying <f00y1n9@gmail.com>
2014-12-25 14:33:43 +0800

    增加状态获取

--- comm/coroutine.py
+++ comm/coroutine.py
@@ -6,7 +6,6 @@ Copyright (c) 2014 Fooying (http://www.fooying.com)
 Mail:f00y1n9[at]gmail.com
 """
 
-import time
 from gevent.pool import Pool
 from gevent.queue import Queue
 from gevent.monkey import patch_all
@@ -19,16 +18,17 @@ class WorkerPool(object):
         self.job_pool = Pool(size=pool_size)
         self.result = Queue()
         self.target_queue = Queue()
+        self.all_job = Pool()
 
     def add_job(self, job_func, *args, **kwargs):
-        self.job_pool.add(
-            self.job_pool.apply_async(
-                job_func,
-                args=args,
-                kwds=kwargs,
-                callback=self._call_func
-            )
-        )
+        job = self.job_pool.apply_async(
+            job_func,
+            args=args,
+            kwds=kwargs,
+            callback=self._call_func)
+        self.job_pool.add(job)
+        self.all_job.add(job)
+
     def run(self, timeout=None):
         self.job_pool.join(timeout=timeout, raise_error=False)
 
@@ -38,11 +38,34 @@ class WorkerPool(object):
 
     def is_finished(self):
         finished = True
-        for job in self.job_pool:
+        for job in self.all_job:
             if not job.ready():
                 finished = False
                 break
         return finished
+
+    def get_pool_wait_num(self):
+        return len(self.job_pool)
+
+    def get_status(self):
+        status = {
+            'total': 0,
+            'wait': 0,
+            'success': 0,
+            'except': 0,
+            'runing': 0
+        }
+        for job in self.all_job:
+            status['total'] += 1
+            if job.started and not job.ready():
+                status['runing'] += 1
+            elif job.successful():
+                status['success'] += 1
+            elif job.ready():
+                status['except'] += 1
+            else:
+                status['wait'] += 1
+        return status
 
     def shutdown(self):
         self.job_pool.kill()
