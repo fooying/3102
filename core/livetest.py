1eb226eff155509b850a23039753352aba708fc8
flsf <jianmaflsf@gmail.com>
2015-01-07 06:18:38 +0800

    --live_test增加获取title

--- core/livetest.py
+++ core/livetest.py
@@ -6,6 +6,8 @@ Copyright (c) 2014 Fooying (http://www.fooying.com)
 Mail:f00y1n9[at]gmail.com
 """
 
+import re
+
 from core.data import result
 from core.data import api
 
@@ -16,17 +18,24 @@ class LiveTest(object):
         self.req = api.request
 
     def __test_targets(self):
+        title_regex = re.compile("<title>(.*?)<\/title>", re.DOTALL|re.M)
         for key in ['root_domain', 'ip', 'domain']:
             for item in result[key]:
                 req = self.req.request('GET', 'http://' + result[key][item]['domain'], timeout=10)
                 result[key][item]['status_code'] = req.status_code
-            if self.exit_flag:
-                break
+                # 标题过长情况要不要考虑一下
+                if req.status_code == 200:
+                    title_match = title_regex.search(req.content)
+                    result[key][item]['title'] = title_match.group(1) if title_match else 'failed'
+                if self.exit_flag:
+                    return
+
 
     def __init_targets(self):
         for key in ['root_domain', 'ip', 'domain']:
             for item in result[key]:
                 result[key][item]['status_code'] = 'unkonwn'
+                result[key][item]['title'] = 'unkonwn'
 
     def exit(self):
         self.exit_flag = True
