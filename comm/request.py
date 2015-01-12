f26a4389072fe1bd97d99370bb85dac9d179bf6e
fooying <f00y1n9@gmail.com>
2015-01-12 11:24:38 +0800

    修改对requests中content的封装 参考http://www.liguangming.com/python-requests-ge-encoding-from-headers 处理requests的编码问题

--- comm/request.py
+++ comm/request.py
@@ -15,7 +15,28 @@ from coroutine import WorkerPool
 
 
 patch_all()
+
 logger = logging.getLogger('3102')
+
+
+def request_patch():
+    prop = requests.models.Response.content
+
+    def content(self):
+        _content = prop.fget(self)
+        if self.encoding == 'ISO-8859-1':
+            encodings = requests.utils.get_encodings_from_content(_content)
+            if encodings:
+                self.encoding = encodings[0]
+            else:
+                self.encoding = self.apparent_encoding
+            _content = _content.decode(self.encoding, 'replace')
+            _content = _content.encode('utf8', 'replace')
+            self._content = _content
+        return _content
+    requests.models.Response.content = property(content)
+
+request_patch()
 
 
 class Req(object):
