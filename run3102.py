a01b8a52e2fa22ad403a82df9c761e3557fef1b6
flsf <jianmaflsf@gmail.com>
2015-02-16 20:22:52 +0800

    minor change

--- run3102.py
+++ run3102.py
@@ -29,7 +29,7 @@ def modulePath():
     except NameError:
         _ = inspect.getsourcefile(modulePath)
 
-    return os.path.dirname(os.path.realpath(getUnicode(_, sys.getfilesystemencoding())))
+    return getUnicode(os.path.dirname(os.path.realpath(_)), sys.getfilesystemencoding())
 
 
 def main(args=None):
